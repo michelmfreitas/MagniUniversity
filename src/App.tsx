4932efd87e0df92a4d8846fd3aa01e38b8d50a8d
Michel Martins <michelmfreitas@gmail.com>
2022-10-14 12:33:19 -0300

    GH Actions

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
   }, [])
 
   return (
-    <Router>
+    <Router basename="magniuniversity.github.io">
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/cursos" element={<Cursos />} />
